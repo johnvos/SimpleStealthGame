b33494091fad3c1dd12bacd1043c7c92cc71d023
John Kim <44280424+johnvos@users.noreply.github.com>
2018-11-11 13:15:21 -0500

    AgentSpawner
    Script for spawning the 2 agents in the beginning.
    Randomly selects one out of ten alcoves for each agent and spawns them in the alcoves.
    AI Agent and Player Agent spawns in different alcoves.
    (Alcove spawn positions hard coded)
    
    Requires
    -Attached Player Agent Prefab
    -Attached AI Agent Prefab

---
+++ Scripts/AgentSpawnerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentSpawnerScript : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject AI;
+
+	void Start () {
+        int playerSpawn = Random.Range(0, 10);
+        int aiSpawn = Random.Range(0, 10);
+        //in case playerSpawn == aiSpawn which will result in both of them spawning at the same exact coordinate
+        while (aiSpawn == playerSpawn) {
+            aiSpawn = Random.Range(0, 10);
+        }
+
+
+        Vector3[] spawnPlacesArray = {
+            //top half
+            new Vector3(-17,0,15),
+            new Vector3(-9,0,15),
+            new Vector3(-1,0,15),
+            new Vector3(7,0,15),
+            new Vector3(15,0,15),
+            //bottom half
+            new Vector3(-15,0,-15),
+            new Vector3(-7,0,-15),
+            new Vector3(1,0,-15),
+            new Vector3(9,0,-15),
+            new Vector3(17,0,-15),
+        };
+
+
+        Spawn(Player, spawnPlacesArray[playerSpawn]);
+        Spawn(AI, spawnPlacesArray[aiSpawn]);
+        
+
+	}
+
+    private void Spawn(GameObject agent, Vector3 spawnPlace) {
+
+        Instantiate(agent, spawnPlace, Quaternion.identity);
+
+    }
+	
+
+
+
+
+}
